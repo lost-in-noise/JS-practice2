65596cbb506b1b90c9682b42eed769eaba5bd7b0
Lost-in-Noise <nancykikvadze@gmail.com>
2023-08-24 00:20:10 +0400

    function

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use strict';
+// ......1
+
+// function sumOfPositiveNumbers(numbers) {
+//     let sum = 0;
+
+//     for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] > 0) {
+//         sum += numbers[i];
+//       }
+//     }
+
+//     return sum;
+//   }
+
+//   const inputNumbers = [2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8];
+//   const result = sumOfPositiveNumbers(inputNumbers);
+//   console.log(result);
+
+//   131
+
+// .....2
+// function calculateSum(numbers) {
+//     let sum = 0;
+
+//     for (let i = 0; i < numbers.length; i++) {
+//       sum += numbers[i];
+//     }
+
+//     return sum;
+//   }
+
+//   const inputNumbers = [10, 50, 6, 7, 8, 11, 6, 3, 9];
+//   const result = calculateSum(inputNumbers);
+//   console.log(result);
+
+//   110
+
+// .....3
